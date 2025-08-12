09e59e98d07546d829e51d6350d5b4b58eb047aa
Michael Kantor <6068672+kantorcodes@users.noreply.github.com>
2025-08-12 12:13:45 -0400

    chore: delete app reference
    Signed-off-by: Michael Kantor <6068672+kantorcodes@users.noreply.github.com>

--- app/src/main/init-logger.ts
+++
@@ -1,4 +0,0 @@
-import { setLoggerFactory } from '@hashgraphonline/standards-sdk'
-import { createElectronLogger } from './utils/electron-logger-adapter'
-
-setLoggerFactory(createElectronLogger)
--- app/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-// Tailwind v4 configuration - minimal config as most settings are in CSS
-export default {
-  content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-}
